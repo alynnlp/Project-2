cb1b56ef0f1b6255aba697f37c5ef7bccb0cfe68
Aileen Pang <aileenpang63@gmail.com>
2017-09-14 22:42:20 +0000

    added cookie and log-in

--- express_server.js
+++ express_server.js
@@ -1,12 +1,15 @@
 //I am working with express and ejs, i need to requrie it from the Node_module
 var express = require("express");
-  var app = express(); //express now is a function
+var cookieParser = require('cookie-parser');
+var app = express(); //express now is a function
 var PORT = process.env.PORT || 8080; // default port 8080, to listen to it so we can view on browser
 app.set("view engine", "ejs") //template needs a template engine
 
 //Middleware - access POST request parameters e.g. req.body.longURL
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieParser());
 
 //req.body.longURL >> which we will store in a var = urlDatabase (later)
 var urlDatabase = {
@@ -27,20 +30,24 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); //is an object
 });
+// //html is not able to show
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello<b>World</b></body></html>");
+// });
 
-
-//this will first give me a blank page until I add something in the body of the ejs template
+//***(1)this will first give me a blank page until I add something in the body of the ejs template
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  console.log(urlDatabase);
+  let templateVars = { urls: urlDatabase,
+    username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 //FORM page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 //a second route
@@ -48,10 +55,14 @@ app.get("/urls/new", (req, res) => {
 // so we need to set the request as a variable = templateVARS
 //
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  //req.params is grabbing the end point of the link above
+  //:id in this case = <%=shortURL%>
+  let templateVars = { shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+      username: req.cookies["username"]};
+      console.log(templateVars);
   res.render("urls_show", templateVars);
 });
-
 
 //template that we created uses method="post". POST request to submit form data.
 //This corresponds with the app.post(...) on the server-side code!
@@ -75,6 +86,18 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//set cookie
+app.post("/urls/login", (req, res) => {
+  //right is where it coming from
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+//logout
+app.post("/urls/logout", (req, res) => {
+  res.clearCookie('username', {path:'/urls/logout' });
+  res.redirect("/urls");
+});
+
 //UPDATE:
 app.post("/urls/:id", (req, res) => {
   //right is where it coming from
@@ -82,10 +105,6 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-//html is not able to show
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello<b>World</b></body></html>");
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
